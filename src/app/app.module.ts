d3ee9a2a471e9093312ff9b9e0210facf162ae7c
Vitaliy Prikota <mr.prikota@gmail.com>
2019-09-22 18:32:37 +0300

    Basic HTML for register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { MatFormFieldModule, MatInputModule, MatRadioModule, MatTreeModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
@@ -21,9 +22,19 @@ const routes: Routes = [
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot(routes),
+        MatInputModule,
+        MatFormFieldModule,
+        MatRadioModule,
+        MatTreeModule,
+    ],
+    exports: [
+        MatInputModule,
+        MatFormFieldModule,
+        MatRadioModule,
+        MatTreeModule,
     ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
