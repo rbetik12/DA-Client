2224b0d9488b24935300160b5a3fc48479f57325
Vitaliy Prikota <mr.prikota@gmail.com>
2019-10-06 20:22:46 +0300

    [READR-23] Fixed dependencies bug, camera correctly works now

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
     selector: 'app-profile-page',
@@ -14,8 +15,12 @@ export class ProfilePageComponent implements OnInit {
     title = 'Profile';
     width: number;
     height: number;
+    currentImage: any;
 
-    constructor(private auth: AuthService, private router: Router, private platform: Platform) {
+    constructor(private auth: AuthService,
+                private router: Router,
+                private platform: Platform,
+                private camera: Camera) {
     }
 
     ngOnInit() {
@@ -33,6 +38,22 @@ export class ProfilePageComponent implements OnInit {
     logout() {
         this.auth.logout();
         this.router.navigateByUrl('/login').then(r => {
+        });
+    }
+
+    takePicture() {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        };
+
+        this.camera.getPicture(options).then((imageData) => {
+            this.currentImage = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+            // Handle error
+            console.log('Camera issue:' + err);
         });
     }
 }
