48eed306e7426d35b3f56c8b55965cb8edb6945d
Vitaliy Prikota <mr.prikota@gmail.com>
2019-10-21 22:45:27 +0300

    [READR-34] Static page with few messages

--- src/app/chat-page/chat-page.component.ts
+++ src/app/chat-page/chat-page.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface MessageModel {
+    sender: string;
+    text: string;
+}
 
 @Component({
     selector: 'app-chat-page',
     templateUrl: './chat-page.component.html',
     styleUrls: ['./chat-page.component.scss'],
 })
-export class ChatPageComponent implements OnInit, OnDestroy {
+export class ChatPageComponent {
+
+    mSender = 'Vitaliy';
+    messages: MessageModel[] = [
+        {
+            sender: 'Vitaliy',
+            text: 'Plasmator fatalis abactor est.Albus adelphis grauiter promissios urbs est.'
+        },
+        {
+            sender: 'Kek',
+            text: 'Cum acipenser messis, omnes fortises experientia velox, regius pulchritudinees.Aonidess trabem, tanquam albus.'
+        },
+        {
+            sender: 'Vitaliy',
+            text: 'Pol, ionicis tormento!Pol, a bene particula, omnia!'
+        }
+    ];
 
     constructor() {
-    }
-
-    ngOnInit() {
-    }
-
-    ngOnDestroy() {
     }
 }
