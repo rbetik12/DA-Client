ec8c8eacd3bc3b50f1ebe19b2f34bb6fc2f3e7c9
Vitaliy <mr.prikota@gmail.com>
2020-01-19 23:52:13 +0300

    Added loading service to likes page

--- src/app/pages/likes-page/likes-page.component.ts
+++ src/app/pages/likes-page/likes-page.component.ts
@@ -4,6 +4,7 @@ import {Endpoints} from '../../endpoints';
 import {UserService} from '../../services/user.service';
 import {User} from '../../models/user.interface';
 import {Like} from '../../models/like.model';
+import {LoadingService} from '../../services/loading.service';
 
 @Component({
     selector: 'app-likes-page',
@@ -15,23 +16,38 @@ export class LikesPageComponent implements OnInit {
     private usersWhoLiked: User[] = [];
 
     constructor(private http: HttpClient,
-                private userService: UserService) {
+                private userService: UserService,
+                private loadingService: LoadingService) {
     }
 
-    ngOnInit() {
+    loading;
+
+    async ngOnInit() {
+        this.loading = await this.loadingService.presentLoading('Loading likes').then(res => {
+            return res;
+        });
+        this.loading.present();
         console.log(this.userService.getUserId());
         this.http.get(Endpoints.userLikes + '/' + this.userService.getUserId()).subscribe(res => {
             this.usersWhoLiked = Object.values(res);
             console.log(this.usersWhoLiked);
+            this.loading.dismiss();
         });
     }
 
-    likeUser(index: number) {
+    async likeUser(index: number) {
+        this.loading = await this.loadingService.presentLoading('Please wait').then(res => {
+            return res;
+        });
+        this.loading.present();
         this.http.post<Like>(Endpoints.like, {
             userId: this.userService.getCredentials()._id,
             userWhoGetLiked: this.usersWhoLiked[index]._id,
         }, {observe: 'response'}).subscribe((res) => {
             console.log(res);
+            this.loading.dismiss();
+        }, error => {
+            this.loading.dismiss();
         });
     }
 }
